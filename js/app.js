da6329b3d8314fbd6dde434b85572a3e94dda7f5
a-minor-threat <dennis.k.shah@gmail.com>
2015-10-16 22:57:33 -0400

    added more stuff

--- js/app.js
+++ js/app.js
@@ -18,48 +18,49 @@ $(document).ready(function() {
 });
 
 function getGuess() {
+    'use strict';
     var guess = parseInt(prompt("Choose a number between 1 and 100"));
     return guess;
 }
 
 function randomNumber() {
-
+    'use strict';
     var randNum = Math.floor(Math.random() * 100);
-    console.log(randNum);
     return randNum;
 }
 
 function feedBack(numDiff) {
-
-  if (numDiff > 80) {
-    alert("You're ice cold!")
-  } else if (numDiff > 60) {
-     alert("You're cold!"); 
-  } else if (numDiff > 40) {
-    alert("You're not cold!");
-  } else if (numDiff > 20) {
-    alert("You're getting slightly warm!");
-  } else if (numDiff > 10){
-    alert("You're getting really warm!");
-  } else if (numDiff>5) { 
-    alert("You're getting hot!");
-  } else if (numDiff>0){
-    alert("You're burning hot!");
-  } else {
-    alert("Burned alive!");
-  }
+    'use strict';
+    if (numDiff > 80) {
+        alert("You're ice cold!");
+    } else if (numDiff > 60) {
+        alert("You're cold!");
+    } else if (numDiff > 40) {
+        alert("You're not cold!");
+    } else if (numDiff > 20) {
+        alert("You're getting slightly warm!");
+    } else if (numDiff > 10) {
+        alert("You're getting really warm!");
+    } else if (numDiff > 5) {
+        alert("You're getting hot!");
+    } else if (numDiff > 0) {
+        alert("You're burning hot!");
+    } else {
+        alert("Burned alive!");
+    }
 }
 
 function newGame() {
+    'use strict';
     var target = randomNumber();
     var guess = -1;
     var guessCount = 0;
     var numDiff;
-    while (target != guess){
-      guess = getGuess();
-      guessCount++;
-      numDiff = Math.abs(target - guess);   
-      feedBack(numDiff);   
+    while (target !== guess) {
+        guess = getGuess();
+        guessCount++;
+        numDiff = Math.abs(target - guess);
+        feedBack(numDiff);
     }
 
 }
