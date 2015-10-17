aa413c4c3e3ef1e7cdd2053ac2665d2c2d24907a
a-minor-threat <dennis.k.shah@gmail.com>
2015-10-17 16:04:44 -0400

    updated HTML and JS

--- js/app.js
+++ js/app.js
@@ -11,56 +11,69 @@ $(document).ready(function() {
         $(".overlay").fadeOut(1000);
     });
 
-    $(".what").click(function() {
+    $(".new").click(function() {
         newGame();
     })
+
+    $(".button").click(function(event) {
+      feedBack();
+    });
 
 });
 
 function getGuess() {
     'use strict';
     var guess = parseInt(prompt("Choose a number between 1 and 100"));
+//    var guess = document.getElementById("userGuess").value;
     return guess;
 }
 
 function randomNumber() {
     'use strict';
     var randNum = Math.floor(Math.random() * 100);
+    console.log(randNum);
     return randNum;
+}
+
+function newGame() {
+    'use strict';
+    console.log("Generating random number and resetting all other variables.")
+    var target = randomNumber();
+    var guess = -1;
+    var guessCount = 0;
+    var numDiff;
+    document.getElementById("count").innerHTML = guessCount;
+    while (target !== guess) {
+        guess = getGuess();
+        guessCount++;
+        document.getElementById("count").innerHTML = guessCount;
+        numDiff = Math.abs(target - guess);
+        feedBack(numDiff);
+    }
+    if (target === guess) {
+        document.getElementById("feedback").innerHTML = "You win!";
+        return;
+    }
 }
 
 function feedBack(numDiff) {
     'use strict';
     if (numDiff > 80) {
-        alert("You're ice cold!");
+        document.getElementById("feedback").innerHTML = "You're ice cold!";
     } else if (numDiff > 60) {
-        alert("You're cold!");
+        document.getElementById("feedback").innerHTML = "You're cold!";
     } else if (numDiff > 40) {
-        alert("You're not cold!");
+        document.getElementById("feedback").innerHTML = "You're not cold!";
     } else if (numDiff > 20) {
-        alert("You're getting slightly warm!");
+        document.getElementById("feedback").innerHTML = "You're getting slightly warm!";
     } else if (numDiff > 10) {
-        alert("You're getting really warm!");
+        document.getElementById("feedback").innerHTML = "You're getting really warm!";
     } else if (numDiff > 5) {
-        alert("You're getting hot!");
+        document.getElementById("feedback").innerHTML = "You're getting hot!";
     } else if (numDiff > 0) {
-        alert("You're burning hot!");
+        document.getElementById("feedback").innerHTML = "You're burning hot!";
     } else {
-        alert("Burned alive!");
+        document.getElementById("feedback").innerHTML = "Burned alive!";
     }
 }
 
-function newGame() {
-    'use strict';
-    var target = randomNumber();
-    var guess = -1;
-    var guessCount = 0;
-    var numDiff;
-    while (target !== guess) {
-        guess = getGuess();
-        guessCount++;
-        numDiff = Math.abs(target - guess);
-        feedBack(numDiff);
-    }
-
-}
